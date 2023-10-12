267aa6abc05b8be3959768bf808eae939f0f9c35
Aj23Mah <ajaymaharjan758@gmail.com>
2023-10-12 17:32:11 +0545

    added and edited css

--- src/API/movie.js
+++ src/API/movie.js
@@ -1,17 +1,17 @@
-import { GetRequest } from "../plugins/http"
+import { GetRequest } from "../plugins/http";
 
-export const APILoadMovies =()=>{
-    return GetRequest('list_movies.json');
-}
+export const APILoadMovies = () => {
+  return GetRequest("list_movies.json");
+};
 
-export const APISearchMovies =(keyword)=>{
-    return GetRequest('list_movies.json?query_term='+keyword);
-}
+export const APISearchMovies = (keyword) => {
+  return GetRequest("list_movies.json?query_term=" + keyword);
+};
 
-export const APISearchMoviesParams =(keyword)=>{
-    return GetRequest('list_movies.json',{
-        params:{
-            query_term: keyword,
-        }
-    });
-}
+export const APISearchMoviesParams = (keyword) => {
+  return GetRequest("list_movies.json", {
+    params: {
+      query_term: keyword,
+    },
+  });
+};
